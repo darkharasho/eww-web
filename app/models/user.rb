1a32f295d3c63e554f94d9c6b50f2c6c4045787f
darkharasho <project96+github@gmail.com>
2023-10-12 13:34:59 -0700

    Fix logout error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[discord]
+         :recoverable, :validatable, :omniauthable, omniauth_providers: %i[discord]
 
   mount_uploader :avatar, AvatarUploader
 
