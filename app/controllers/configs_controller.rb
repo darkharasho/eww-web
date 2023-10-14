1e8cda8763f0d5dfa21d20e78dd98f3da4a18fe1
darkharasho <project96+github@gmail.com>
2023-10-14 15:06:14 -0700

    various bug fixes, working validation

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -6,16 +6,55 @@ class ConfigsController < ApplicationController
     @config = Config.find(params[:id])
 
     if %w[raid_notification build_manager_role_ids allowed_admin_role_ids raid_reminder].include? @config.name
-      @role_multiselect = @config.guild.roles.map do |role|
-        [role["name"], role["id"]]
+      roles = @config.guild.roles
+      if roles.class == Hash && roles.dig("message") == "You are being rate limited."
+        redirect_to guild_path(@config.guild), alert: "Rate limit reached. Try again later."
+        return
+      else
+        @role_multiselect = roles.map do |role|
+          [role["name"], role["id"]]
+        end
       end
     end
-    if %w[auto_attendance raid_reminder].include? @config.name
-      @channel_select = @config.guild.text_channels.map{|channel| [channel["name"], channel["id"]]}
+    if %w[auto_attendance raid_reminder].include?(@config.name)
+      text_channels = @config.guild.text_channels
+      if text_channels.class == Hash && text_channels.dig("message") == "You are being rate limited."
+        redirect_to guild_path(@config.guild), alert: "Rate limit reached. Try again later."
+        return
+      else
+        @channel_select = text_channels.map{|channel| [channel["name"], channel["id"]]}
+      end
     end
 
     if %w[raid_reminder].include? @config.name
       @build_classes = @config.guild.build_classes
+      store_selected = []
+
+      options = ''
+      options << ActionController::Base.helpers.content_tag(:optgroup, label: "Guild Builds", "data-closable" => "close") do ||
+        @build_classes.each do |item|
+          selected = @config.value["classes"].any? { |cls| cls == item }
+          store_selected << item if selected
+          ActionController::Base.helpers.concat(ActionController::Base.helpers.content_tag(:option, item, value: item, selected: selected))
+        end
+      end
+
+      options << ActionController::Base.helpers.content_tag(:optgroup, label: "Other GW2 Classes", "data-closable" => "close") do ||
+        GuildHelper.all_classes.reject{ |cls| @build_classes.include?(cls) }.each do |item|
+          selected = @config.value["classes"].any? { |cls| cls == item }
+          ActionController::Base.helpers.concat(ActionController::Base.helpers.content_tag(:option, item, value: item, selected: selected))
+        end
+      end
+
+      options << ActionController::Base.helpers.content_tag(:optgroup, label: "Other", "data-closable" => "close") do ||
+        ["Other", "None of the Above", "Not Listed"].each do |item|
+          selected = @config.value["classes"].any? { |cls| cls == item }
+          selected = false if store_selected.any? { |cls| cls == item }
+          store_selected << item if selected
+          ActionController::Base.helpers.concat(ActionController::Base.helpers.content_tag(:option, item, value: item, selected: selected))
+        end
+      end
+      @guild_build_options = options.html_safe
     end
 
     render :edit
@@ -30,7 +69,8 @@ class ConfigsController < ApplicationController
       elsif @config.update value: eval(config_params[:value])
         format.html { redirect_to @config.guild, notice: 'Config updated successfully' }
       else
-        format.html { redirect_to guild_path(@config.guild)}
+        # format.html { redirect_to edit_config_path(@config), alert: @config.errors.full_messages.map{|e| "<p>#{e}</p>"}.join(" ")}
+        format.html { redirect_to edit_config_path(@config), alert: @config.errors.full_messages.join(" | ")}
       end
     end
   end
@@ -55,18 +95,31 @@ class ConfigsController < ApplicationController
     when "raid_notification"
       params[:config] = {}
       params[:config][:value] = {
-        role_ids: params["role_ids"].map(&:to_i),
-        open_tag_role_ids: params["open_tag_role_ids"].map(&:to_i)
+        role_ids: params["role_ids"]&.map(&:to_i),
+        open_tag_role_ids: params["open_tag_role_ids"]&.map(&:to_i)
       }.to_s
     when "auto_attendance"
       params[:config] = {}
       params[:config][:value] = {
         enabled: params["enabled"],
-        channel_id: params["channel_id"].to_i,
+        channel_id: params["channel_id"] ? params["channel_id"].to_i : params["channel_id"],
         time: {
           hour: params["time"]["time(4i)"],
           minute: params["time"]["time(5i)"]
         }
+      }.to_s
+    when "raid_reminder"
+      params[:config] = {}
+      params[:config][:value] = {
+        hide_empty_rows: params["hide_empty_rows"],
+        table_style: params["table_style"],
+        channel_id: params["channel_id"] ? params["channel_id"].to_i : params["channel_id"],
+        time: {
+          hour: params["time"]["time(4i)"],
+          minute: params["time"]["time(5i)"]
+        },
+        role_ids: params["role_ids"]&.map(&:to_i),
+        classes: params["build_classes"]
       }.to_s
     end
     params
--- app/lib/guild_helper.rb
+++ app/lib/guild_helper.rb
@@ -12,4 +12,87 @@ class GuildHelper
       end
     end
   end
+
+  def self.all_classes
+    %w[Mesmer Mirage Chronomancer Virtuoso Guardian Dragonhunter Firebrand Willbender Necromancer Reaper Scourge Harbinger Ranger Druid Soulbeast Untamed Elementalist Tempest Weaver Catalyst Warrior Berserker Spellbreaker Bladesworn Thief Daredevil Deadeye Specter Engineer Holosmith Scrapper Mechanist Revenant Renegade Herald Vindicator]
+  end
+
+  def self.determine_class_icon(klass)
+    case klass.downcase
+    when /reaper/
+      ActionController::Base.helpers.image_tag("class_icons/Reaper.png", class: "gw2-class-icon")
+    when /scourge/
+      ActionController::Base.helpers.image_tag("class_icons/Scourge.png", class: "gw2-class-icon")
+    when /necro/, /necromancer/
+      ActionController::Base.helpers.image_tag("class_icons/Necromancer.png", class: "gw2-class-icon")
+    when /harb/, /harbinger/
+      ActionController::Base.helpers.image_tag("class_icons/Harbinger.png", class: "gw2-class-icon")
+    when /chronomancer/, /chrono/
+      ActionController::Base.helpers.image_tag("class_icons/Chronomancer.png", class: "gw2-class-icon")
+    when /mirage/
+      ActionController::Base.helpers.image_tag("class_icons/Mirage.png", class: "gw2-class-icon")
+    when /virt/, /virtuoso/
+      ActionController::Base.helpers.image_tag("class_icons/Virtuoso.png", class: "gw2-class-icon")
+    when /mes/, /mesmer/
+      ActionController::Base.helpers.image_tag("class_icons/Mesmer.png", class: "gw2-class-icon")
+    when /rev/, /revenant/
+      ActionController::Base.helpers.image_tag("class_icons/Revenant.png", class: "gw2-class-icon")
+    when /renegade/, /rene/
+      ActionController::Base.helpers.image_tag("class_icons/Renegade.png", class: "gw2-class-icon")
+    when /herald/
+      ActionController::Base.helpers.image_tag("class_icons/Herald.png", class: "gw2-class-icon")
+    when /vindicator/, /vindi/, /vindy/, /ventari/
+      ActionController::Base.helpers.image_tag("class_icons/Vindicator.png", class: "gw2-class-icon")
+    when /guard/, /guardian/
+      ActionController::Base.helpers.image_tag("class_icons/Guardian.png", class: "gw2-class-icon")
+    when /firebrand/, /fb/, /boonbrand/, /healbrand/
+      ActionController::Base.helpers.image_tag("class_icons/Firebrand.png", class: "gw2-class-icon")
+    when /dh/, /dragonhunter/
+      ActionController::Base.helpers.image_tag("class_icons/Dragonhunter.png", class: "gw2-class-icon")
+    when /wb/, /willbender/
+      ActionController::Base.helpers.image_tag("class_icons/Willbender.png", class: "gw2-class-icon")
+    when /engi/, /engineer/
+      ActionController::Base.helpers.image_tag("class_icons/Engineer.png", class: "gw2-class-icon")
+    when /scrapper/
+      ActionController::Base.helpers.image_tag("class_icons/Scrapper.png", class: "gw2-class-icon")
+    when /holo/, /holosmith/
+      ActionController::Base.helpers.image_tag("class_icons/Holosmith.png", class: "gw2-class-icon")
+    when /mech/, /mechanist/
+      ActionController::Base.helpers.image_tag("class_icons/Mechanist.png", class: "gw2-class-icon")
+    when /ranger/
+      ActionController::Base.helpers.image_tag("class_icons/Ranger.png", class: "gw2-class-icon")
+    when /soulbeast/, /sb/
+      ActionController::Base.helpers.image_tag("class_icons/Soulbeast.png", class: "gw2-class-icon")
+    when /druid/
+      ActionController::Base.helpers.image_tag("class_icons/Druid.png", class: "gw2-class-icon")
+    when /untamed/
+      ActionController::Base.helpers.image_tag("class_icons/Untamed.png", class: "gw2-class-icon")
+    when /ele/, /elementalist/
+      ActionController::Base.helpers.image_tag("class_icons/Elementalist.png", class: "gw2-class-icon")
+    when /tempest/
+      ActionController::Base.helpers.image_tag("class_icons/Tempest.png", class: "gw2-class-icon")
+    when /weaver/
+      ActionController::Base.helpers.image_tag("class_icons/Weaver.png", class: "gw2-class-icon")
+    when /cata/, /catalyst/
+      ActionController::Base.helpers.image_tag("class_icons/Catalyst.png", class: "gw2-class-icon")
+    when /war/, /warrior/
+      ActionController::Base.helpers.image_tag("class_icons/Warrior.png", class: "gw2-class-icon")
+    when /spb/, /spellbreaker/
+      ActionController::Base.helpers.image_tag("class_icons/Spellbreaker.png", class: "gw2-class-icon")
+    when /zerker/, /berserker/
+      ActionController::Base.helpers.image_tag("class_icons/Berserker.png", class: "gw2-class-icon")
+    when /bs/, /bladesworn/
+      ActionController::Base.helpers.image_tag("class_icons/Bladesworn.png", class: "gw2-class-icon")
+    when /thief/, /teef/
+      ActionController::Base.helpers.image_tag("class_icons/Thief.png", class: "gw2-class-icon")
+    when /deadeye/
+      ActionController::Base.helpers.image_tag("class_icons/Deadeye.png", class: "gw2-class-icon")
+    when /daredevil/, /dd/
+      ActionController::Base.helpers.image_tag("class_icons/Daredevil.png", class: "gw2-class-icon")
+    when /specter/, /spectre/
+      ActionController::Base.helpers.image_tag("class_icons/Specter.png", class: "gw2-class-icon")
+    else
+      "<span>✅</span>"
+    end
+  end
 end
